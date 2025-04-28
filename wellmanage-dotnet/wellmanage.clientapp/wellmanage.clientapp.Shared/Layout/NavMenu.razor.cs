4b64712e93947a93fd33047f1abb7fee046c68f2
Sadman Jahin <sadman.jahin@welldev.io>
2025-04-28 06:10:01 -0700

    attendence fr intiial

---
+++ wellmanage-dotnet/wellmanage.clientapp/wellmanage.clientapp.Shared/Layout/NavMenu.razor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using wellmanage.clientapp.Shared.Services;
+using wellmanage.shared.Models;
+
+namespace wellmanage.clientapp.Shared.Layout
+{
+    public partial class NavMenu
+    {
+        [Inject]
+        AttendenceService attendenceService { get; set; }
+        AttendanceStatus currentAttendenceStatus = new AttendanceStatus();
+
+        protected override void OnInitialized()
+        {
+            attendenceService.OnAttendenceChanged += (attendenceStatus) =>
+            {
+                currentAttendenceStatus = attendenceStatus;
+                StateHasChanged();
+            };
+        }
+        public async Task CheckIn()
+        {
+           await attendenceService.CheckInUser();
+        }
+        public async Task CheckOut()
+        {
+            await attendenceService.CheckOutUser();
+        }
+    }
+}
---
+++ wellmanage-dotnet/wellmanage.clientapp/wellmanage.clientapp.Shared/Services/AttendenceService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using wellmanage.shared.Models;
+
+namespace wellmanage.clientapp.Shared.Services
+{
+    public class AttendenceService
+    {
+        private readonly HttpClient _http;
+        public event Action<AttendanceStatus> OnAttendenceChanged;
+
+        public AttendenceService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<AttendanceStatus> GetAttendenceStatus()
+        {
+            var response = await _http.GetAsync("api/v1/user/attendence-status");
+            var result = await response.Content.ReadFromJsonAsync<AttendanceStatus>();
+            OnAttendenceChanged?.Invoke(result);
+            return result;
+        } 
+
+        public async Task CheckInUser()
+        {
+            var response = await _http.PostAsync("api/v1/user/check-in", null);
+        }
+
+        public async Task CheckOutUser()
+        {
+            var response = await _http.PostAsync("api/v1/user/check-out", null);
+        }
+    }
+}
