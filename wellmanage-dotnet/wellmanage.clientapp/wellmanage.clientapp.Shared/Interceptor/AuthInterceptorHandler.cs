1694f682931ec7c16460d428f596a1ea7375baea
Sadman Jahin <sadman.jahin@welldev.io>
2025-05-12 11:22:37 +0600

    navigation to login if 401

--- wellmanage-dotnet/wellmanage.clientapp/wellmanage.clientapp.Shared/Interceptor/AuthInterceptorHandler.cs
+++ wellmanage-dotnet/wellmanage.clientapp/wellmanage.clientapp.Shared/Interceptor/AuthInterceptorHandler.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using wellmanage.clientapp.Shared.Interfaces;
+using wellmanage.clientapp.Shared.Services;
 using wellmanage.shared.Models;
 
 namespace wellmanage.clientapp.Shared.Interceptor
@@ -13,10 +17,13 @@ namespace wellmanage.clientapp.Shared.Interceptor
     public class AuthorizationHandler : DelegatingHandler
     {
         private readonly IAppStorage _appStorage;
+        private readonly NavigationManager _navigation;
+        private readonly JwtAuthStateProvider _authStateProvider;
 
-        public AuthorizationHandler(IAppStorage appStorage)
+        public AuthorizationHandler(IAppStorage appStorage, AuthenticationStateProvider authStateProvider)
         {
             _appStorage = appStorage;
+            _authStateProvider = (JwtAuthStateProvider)authStateProvider;
         }
 
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
@@ -34,7 +41,16 @@ namespace wellmanage.clientapp.Shared.Interceptor
                     request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
                 }
             }
-            return await base.SendAsync(request, cancellationToken);
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                await _authStateProvider.MarkUserAsLoggedOut();
+                _navigation.NavigateTo("/login", true);
+            }
+
+            return response;
         }
     }
 
