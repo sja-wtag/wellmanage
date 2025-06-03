6781087cecb493fcf3f5a9283f1ef48a3bdd4d12
Sadman Jahin <sadman.jahin@welldev.io>
2025-06-03 15:07:30 +0600

    emp update

---
+++ wellmanage-dotnet/wellmanage.dataaccess/Migrations/20250603085744_Employee-Alter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace wellmanage.data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeAlter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmployeeProject",
+                columns: table => new
+                {
+                    EmployeesId = table.Column<long>(type: "bigint", nullable: false),
+                    ProjectsProjectId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeeProject", x => new { x.EmployeesId, x.ProjectsProjectId });
+                    table.ForeignKey(
+                        name: "FK_EmployeeProject_Employees_EmployeesId",
+                        column: x => x.EmployeesId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeeProject_Projects_ProjectsProjectId",
+                        column: x => x.ProjectsProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmployeeProject_ProjectsProjectId",
+                table: "EmployeeProject",
+                column: "ProjectsProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeeProject");
+        }
+    }
+}
